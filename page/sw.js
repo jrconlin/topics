5ec34a28cb746e9fd59909d2ce8d5686d4fc3dc2
Kit Cambridge <kit@yakshaving.ninja>
2016-10-28 00:16:00 -0700

    page: Simplify the worker and test page a bit.
    * We can use `self.registration.showNotification()` to show a
      notification from the worker. Now that service workers exist,
      `showNotification()` is the preferred way to do this; the
      `Notification` constructor is only for non-persistent notifications.
      It also works even when the site isn't open in a tab
      (`self.clients.matchAll` won't return any matches), and avoids
      round-tripping back to the page.
    * Promises can be composed just like Twisted's deferreds: if a `.then`
      or `.catch` handler returns a promise, subsequent handlers will wait
      on it. This means we can flatten the handler chains in `register`
      and `subscribe`.
    * Throwing inside a `.then` handler rejects the promise, and executes
      the next `.catch` handler. This lets us tighten up the error handling
      logic.
    * We can tighten up the `fetch` call, too. No need to manually
      construct `Request` or `Headers` objects; `fetch` will take care of
      that for us. `JSON.stringify` will also automatically call
      `toJSON()`.
    * `response.ok` is a nice shortcut for "is the response a 2xy status?"
    * Since we use `clients.claim()` and `skipWaiting()` when installing
      the service worker, we can use `navigator.serviceWorker.ready` after
      registration. No need to reload the page. \o/
    * `JSON.stringify(event)` hides most of the event's properties, because
      they're not enumerable. If we just pass the event directly to
      `console.*`, our console can inspect it.

--- page/sw.js
+++ page/sw.js
@@ -9,7 +9,7 @@ self.addEventListener('push', function(event)  {
        They should include a .data component that is the decrypted
        content of the message.
     */
-    console.info("**** Recv'd a push message::", JSON.stringify(event));
+    console.info("**** Recv'd a push message::", event);
 
     if (event.data) {
         // Data is a accessor. Data may be in one of several formats.
@@ -25,23 +25,11 @@ self.addEventListener('push', function(event)  {
         console.log("Service worker just got:", content);
         // Send the event to the parent pages.
         event.waitUntil(
-          self.clients.matchAll()
-           .then(clientList => {
-              let sent = false;
-              console.debug("Service worker found clients",
-                    JSON.stringify(clients));
-             clientList.forEach(client => {
-                 console.debug("Service worker sending to client...", client);
-                 sent = true;
-                 client.postMessage({'type':'content','content':content});
-             });
-             if (sent == false) {
-                 throw new Error("No valid client to send to.");
-             }
-           })
-           .catch(err => {
-              console.error("Service worker couldn't send message: ", err);
-           })
+          self.registration.showNotification("Got message", {
+            body: content,
+            icon: "icon.png",
+            //tag: "collapse tag",  // This will collapse all notifications with this tag name
+          })
         );
     }
 });
@@ -49,35 +37,10 @@ self.addEventListener('push', function(event)  {
 /* The following are other events that this service worker could respond to.
  */
 
-self.addEventListener('message', function(event) {
-    // A message has been sent to this service worker.
-    console.log("sw Handling message event:", event);
-});
-
 self.addEventListener('pushsubscriptionchange', function(event) {
     // The Push subscription ID has changed. The App should send this
     // information back to the App Server.
     console.log("sw Push Subscription Change", event);
-    event.waitUntil(
-        self.clients.matchAll()
-           .then(clientList => {
-             let sent = false;
-             console.debug("Service worker found clients",
-                    JSON.stringify(clients));
-             clientList.forEach(client => {
-                 console.debug("Service worker sending to client...", client);
-                 sent = true;
-                 client.postMessage({'type':'update'});
-             });
-             if (sent == false) {
-                 throw new Error("No valid client to send to.");
-             }
-           })
-           .catch(err => {
-              console.error("Service worker couldn't send message: ", err);
-           })
-        );
-
 });
 
 self.addEventListener('registration', function(event){
@@ -89,20 +52,19 @@ self.addEventListener('registration', function(event){
 self.addEventListener('install', function(event){
     // The serivce worker has been loaded and installed.
     // The browser aggressively caches the service worker code.
-    console.log("sw Install: ", JSON.stringify(event));
+    console.log("sw Install: ", event);
     // This replaces currently active service workers with this one
     // making this service worker a singleton.
     event.waitUntil(self.skipWaiting());
-    console.log("sw Installed: ", JSON.stringify(event));
+    console.log("sw Installed: ", event);
 
 });
 
 self.addEventListener('activate', function(event){
     // The service worker is now Active and functioning.
-    console.log("sw Activate : ", JSON.stringify(event));
+    console.log("sw Activate : ", event);
     // Again, ensure that this is the only active service worker for this
     // page.
     event.waitUntil(self.clients.claim());
-    console.log("sw Activated: ", JSON.stringify(event));
-    navigator.serviceWorker
+    console.log("sw Activated: ", event);
 });
